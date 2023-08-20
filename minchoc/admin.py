00005f26590d55b27b60f13b6150e36b9165a80c
Andrew Udvare <audvare@gmail.com>
2023-08-20 18:29:06 -0400

    minchoc/admin: register Author

--- minchoc/admin.py
+++ minchoc/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Company, NugetUser, Package
+from .models import Author, Company, NugetUser, Package
 
+admin.site.register(Author)
 admin.site.register(Company)
 admin.site.register(NugetUser)
 admin.site.register(Package)
