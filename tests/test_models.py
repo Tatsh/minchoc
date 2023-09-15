b0b25fd808095ff37217e48c1e27e94a2c3905cf
Andrew Udvare <audvare@gmail.com>
2023-09-15 10:38:56 -0400

    tests/test_models: add test for user create hook

--- tests/test_models.py
+++ tests/test_models.py
@@ -31,3 +31,18 @@ def test_package_str(nuget_user: NugetUser) -> None:
 def test_nuget_user_str(nuget_user: NugetUser) -> None:
     nuget_user.base.username = 'fakename'
     assert str(nuget_user) == 'fakename'
+
+
+@pytest.mark.django_db
+def test_only_one_nuget_user_is_created_on_first_save() -> None:
+    from django.contrib.auth.models import User
+
+    from minchoc.models import NugetUser
+    user = User.objects.create()
+    assert user is not None
+    nuget_user = NugetUser.objects.filter(base=user).first()
+    assert nuget_user is not None
+    user.save()
+    nuget_users = NugetUser.objects.filter(base=user)
+    assert nuget_users[0] == nuget_user
+    user.delete()
